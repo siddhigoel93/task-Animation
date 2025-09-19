adb200b2682ecc891fe9ab4ff7d30157fff1d21d
Siddhi Goel <siddhigoel93@gmail.com>
2025-09-20 01:07:37 +0530

    create blue balls randomly

---
+++ script.js
@@ -0,0 +1,19 @@
+const arena = document.querySelector('.arena');
+const start = document.getElementById('start');
+const reset = document.getElementById('reset');
+const score = document.getElementById('score');
+const time = document.getElementById('time');
+const end = document.getElementById('end');
+const fscore = document.getElementById('fscore');
+
+function ballcreation(){
+    const ball = document.createElement('div');
+    ball.classList.add('blueball');
+
+    const x = Math.random() * (arena.clientWidth - 30);
+    const y = Math.random() * (arena.clientHeight - 30);
+    ball.style.left = `${x}px`;
+    ball.style.top = `${y}px`;
+    arena.appendChild(ball);
+    
+}
